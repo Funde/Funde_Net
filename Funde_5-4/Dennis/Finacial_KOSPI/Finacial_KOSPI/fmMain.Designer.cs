aae8629f1312574bdb424834f2c2c2fc4749011a
DennisJung <hyunsukdn@gmail.com>
2013-05-25 04:20:12 +0900

    Dennis Financil Project Commit

--- Funde_5-4/Dennis/Finacial_KOSPI/Finacial_KOSPI/fmMain.Designer.cs
+++ Funde_5-4/Dennis/Finacial_KOSPI/Finacial_KOSPI/fmMain.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.b_HtmlLoad = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // b_HtmlLoad
+            // 
+            this.b_HtmlLoad.Location = new System.Drawing.Point(292, 30);
+            this.b_HtmlLoad.Name = "b_HtmlLoad";
+            this.b_HtmlLoad.Size = new System.Drawing.Size(86, 41);
+            this.b_HtmlLoad.TabIndex = 0;
+            this.b_HtmlLoad.Text = "Load";
+            this.b_HtmlLoad.UseVisualStyleBackColor = true;
+            this.b_HtmlLoad.Click += new System.EventHandler(this.b_HtmlLoad_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(45, 38);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 21);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.Value = new System.DateTime(2013, 5, 24, 0, 0, 0, 0);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(86, 86);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(246, 320);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "체결시각";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "체결가";
+            this.Column2.Name = "Column2";
             // 
             // fmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(719, 418);
+            this.ClientSize = new System.Drawing.Size(423, 418);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.b_HtmlLoad);
             this.Name = "fmMain";
             this.Text = "KOSPI 프로그램";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button b_HtmlLoad;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
     }
 }
 
--- Funde_5-4/Dennis/Finacial_KOSPI/Finacial_KOSPI/fmMain.cs
+++ Funde_5-4/Dennis/Finacial_KOSPI/Finacial_KOSPI/fmMain.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using System.Net;
+using System.IO;
+
+
 namespace Finacial_KOSPI
 {
     public partial class fmMain : Form
@@ -14,6 +18,52 @@ namespace Finacial_KOSPI
         public fmMain()
         {
             InitializeComponent();
+
+            dateTimePicker1.CustomFormat = "yyyyMMdd";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+        }
+
+        private void b_HtmlLoad_Click(object sender, EventArgs e)
+        {
+            
+            const string htmldate = "<td class=\"date\">";
+            const string htmlnumber = "<td class=\"number_1\">";
+            const string htmlend    = "</td>";
+            String dates = "";
+            dates = dateTimePicker1.Text;
+            dataGridView1.Rows.Clear();
+            for (int i = 1; i <= 60; i++)
+            {
+                String vURL = "http://finance.naver.com/sise/sise_index_time.nhn?code=KOSPI&thistime="+ dates +"185901&page=" + i.ToString();
+                HttpWebRequest oRequest = (HttpWebRequest)WebRequest.Create(vURL);
+                HttpWebResponse oGetResponse = (HttpWebResponse)oRequest.GetResponse();
+                StreamReader oStreamReader = new StreamReader(oGetResponse.GetResponseStream(), Encoding.GetEncoding("ks_c_5601-1987"), true);
+                string strHtml = oStreamReader.ReadToEnd();
+                int j = 0;
+                
+                while((j = strHtml.IndexOf(htmldate, j)) != -1)
+                {
+                    int startpos, endpos;
+                    string[] context = new string[2];
+                    startpos    = j + htmldate.Length;
+                    endpos      = strHtml.IndexOf(htmlend, j);
+                    
+                    context[0] = strHtml.Substring(startpos, endpos-startpos);
+                    
+                    j = endpos;
+
+                    j = strHtml.IndexOf(htmlnumber, j);
+                    startpos    = j + htmlnumber.Length;
+                    endpos      = strHtml.IndexOf(htmlend, j);
+
+
+                    context[1] = strHtml.Substring(startpos, endpos - startpos);
+                    dataGridView1.Rows.Add(context);
+                }
+            }
+            
+//dataGridView1.Row
+            
         }
     }
 }
